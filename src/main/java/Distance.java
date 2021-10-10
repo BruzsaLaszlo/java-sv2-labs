e6a47cabfd66e6c6471a1564f234270e8f9eee0e
Laika007 <lacika007@gmail.com>
2021-10-10 20:10:02 +0200

    Distance

---
+++ src/main/java/Distance.java
@@ -0,0 +1,19 @@
+public class Distance {
+
+    private double distanceInKm;
+    private boolean exact;
+
+
+    public Distance(double distanceInKm, boolean exact) {
+        this.distanceInKm = distanceInKm;
+        this.exact = exact;
+    }
+
+    public double getDistanceInKm() {
+        return distanceInKm;
+    }
+
+    public boolean isExact() {
+        return exact;
+    }
+}
---
+++ src/main/java/DistanceMain.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class DistanceMain {
+
+    public static void main(String[] args) {
+
+        Distance distance = new Distance(3.33, false);
+
+        System.out.println("Distance: " + distance.getDistanceInKm());
+        System.out.println("Exact: " + distance.isExact());
+
+        int i = (int) distance.getDistanceInKm();
+        System.out.println("Egészrész: " + i);
+    }
+}

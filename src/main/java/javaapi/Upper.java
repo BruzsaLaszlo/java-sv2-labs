bf3243c520b8cb70a4a7d83581df6ad5ee9625e7
Laika007 <lacika007@gmail.com>
2021-10-14 21:25:26 +0200

    Java API

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,15 @@
+package javaapi;
+
+/**
+ * Keresd ki a JDK API dokumentációból, a String osztálynál, hogyan lehet egy karakterláncot nagybetűssé tenni!
+ * <p>
+ * Írj egy Upper osztályt, ami a Hello World! szöveget nagybetűssé alakítja!
+ */
+
+public class Upper {
+
+    public static void main(String[] args) {
+        String hw = "Hello World";
+        hw.toUpperCase();
+    }
+}

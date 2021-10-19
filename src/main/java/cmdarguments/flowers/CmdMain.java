c33ce6b1b1c005552965105d97550e81f2ab0851
Laci <lacika007@gmail.com>
2021-10-20 00:21:26 +0200

    Parancssori paraméterek

---
+++ src/main/java/cmdarguments/flowers/CmdMain.java
@@ -0,0 +1,10 @@
+package cmdarguments.flowers;
+
+public class CmdMain {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++)
+            System.out.println(i + 1 + ". elem: " + args[i]);
+    }
+
+}

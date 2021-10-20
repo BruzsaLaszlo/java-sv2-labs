0e884dd013a3520e8996b467cd265b0e38bd275b
Laci <lacika007@gmail.com>
2021-10-20 18:22:01 +0200

    Lista

---
+++ src/main/java/arrays/arraylist/Freezer.java
@@ -0,0 +1,22 @@
+package arrays.arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Freezer {
+
+    public static void main(String[] args) {
+
+        List<String> etel = new ArrayList<>();
+        etel.add("csirke mell");
+        etel.add("pacal");
+        etel.add("hal");
+        etel.add("máj");
+        etel.add("bab");
+
+        System.out.println(etel);
+        System.out.println("Méret : " + etel.size());
+
+    }
+
+}
---
+++ src/main/java/arrays/arraylist/Second.java
@@ -0,0 +1,16 @@
+package arrays.arraylist;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Second {
+
+    public static void main(String[] args) {
+
+        List<Integer> nums = Arrays.asList(3, 9, 42, 67, 4, 0, 76, 14, 84, 35, 92);
+        for (int i = 0; i < nums.size(); i++)
+            if (i % 2 == 0)
+                System.out.print(nums.get(i) + " ");
+
+    }
+}

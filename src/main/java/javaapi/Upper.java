8acca8759ba71725a44703f904be2189e15b8893
Laci <lacika007@gmail.com>
2021-10-21 21:14:46 +0200

    Java API

--- src/main/java/javaapi/Upper.java
+++ src/main/java/javaapi/Upper.java
@@ -9,7 +9,9 @@ package javaapi;
 public class Upper {
 
     public static void main(String[] args) {
+
         String hw = "Hello World";
-        hw.toUpperCase();
+        System.out.println(hw.toUpperCase());
+
     }
 }

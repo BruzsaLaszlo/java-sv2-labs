6ddced498bc899e83071a802304e55682b82e891
Laci <lacika007@gmail.com>
2021-10-21 21:33:40 +0200

    Sortörés kezelése

--- src/main/java/linebreak/Car.java
+++ src/main/java/linebreak/Car.java
@@ -1,13 +1,19 @@
 package linebreak;
 
 public class Car {
+
     public String getBrandAndTypeInSeparateLines(String brand, String type) {
         return brand + "\n" + type;
     }
 
     public static void main(String[] args) {
+
         Car car = new Car();
+
         String lines = car.getBrandAndTypeInSeparateLines("Skoda", "Octavia");
+
         System.out.println(lines);
+
     }
+
 }
--- src/main/java/linebreak/CityNames.java
+++ src/main/java/linebreak/CityNames.java
@@ -1,9 +1,13 @@
 package linebreak;
 
 public class CityNames {
+
     public static void main(String[] args) {
+
         System.out.print("Cegléd\n" +
                 "Albertirsa\n" +
                 "Kecskemét");
+
     }
+
 }

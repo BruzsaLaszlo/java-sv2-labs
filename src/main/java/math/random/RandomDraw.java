128df710910d6626bf1ac695af87bd638c50a097
Laci <lacika007@gmail.com>
2021-10-22 00:36:19 +0200

    Math és Random osztály

--- src/main/java/math/random/RandomDraw.java
+++ src/main/java/math/random/RandomDraw.java
@@ -8,13 +8,13 @@ public class RandomDraw {
 
     public static void main(String[] args) {
 
-        List<String> names = Arrays.asList("MindegyNemNyert",
-                "Zoli", "Laci", "Peti", "Ica", "Eszti", "Zsolti", "Bandi", "Andi", "Joci");
+        List<String> names = Arrays.asList(
+                "Pisti", "Zoli", "Laci", "Peti", "Ica", "Eszti", "Zsolti", "Bandi", "Andi", "Joci");
 
         Random random = new Random();
 
-        System.out.println(names.get(random.nextInt(5) + 1));
-        System.out.println(names.get(random.nextInt(5) + 6));
+        System.out.println(names.get(random.nextInt(5)));
+        System.out.println(names.get(random.nextInt(5) + 5));
 
     }
 

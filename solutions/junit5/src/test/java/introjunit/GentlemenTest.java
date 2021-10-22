709462169f6ae07cae63c88366d99bf1359b9741
Laci <lacika007@gmail.com>
2021-10-22 02:51:31 +0200

    junit5

---
+++ solutions/junit5/src/test/java/introjunit/GentlemenTest.java
@@ -0,0 +1,31 @@
+package introjunit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GentlemenTest {
+
+    @Test
+    void testWelcome() {
+
+        Gentlemen gentlemen = new Gentlemen();
+
+        String welcome = gentlemen.sayHello("John Doe");
+
+        assertEquals("Hello John Doe", welcome);
+
+    }
+
+    @Test
+    void testWelcomeNull() {
+
+        Gentlemen gentlemen = new Gentlemen();
+
+        String welcome = gentlemen.sayHello(null);
+
+        assertEquals("Hello John Doe", welcome);
+
+    }
+
+}

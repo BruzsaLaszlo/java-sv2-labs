8a8a2654888c46c357f3c407fb0c973cdb3ba928
Bruzsa László <lacika007@gmail.com>
2021-10-28 23:03:38 +0200

    Főbb String metódusok

--- src/main/java/stringmethods/url/UrlManager.java
+++ src/main/java/stringmethods/url/UrlManager.java
@@ -14,7 +14,7 @@ public class UrlManager {
 
     public static void main(String[] args) {
 
-        String url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02";
+        String url = "https://earthquake.usgs.gov:80/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02";
         UrlManager urlManager = new UrlManager(url);
 
         System.out.println(urlManager.getProtocolFromUrl());

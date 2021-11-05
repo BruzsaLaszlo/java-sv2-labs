402bbb563fafadbf5b1cd0926cb4e4462901b94a
Bruzsa László <lacika007@gmail.com>
2021-11-05 23:25:07 +0100

    Tömbök

--- src/main/java/array/ArrayHandler.java
+++ src/main/java/array/ArrayHandler.java
@@ -3,43 +3,52 @@ package array;
 public class ArrayHandler {
 
     public void addIndexToNumber(int[] source) {
+
         for (int i = 0; i < source.length; i++)
             source[i] += i;
+
     }
 
     public void concatenateIndexToWord(String[] source) {
+
         for (int i = 0; i < source.length; i++)
             source[i] = i + ". " + source[i];
+
     }
 
+
     public static void main(String[] args) {
+
         ArrayHandler arrayHandler = new ArrayHandler();
 
-        int[] intArray = {1, 1, 1, 1, 10};
-        String[] stringArray = {"S", "M", "L", "XL", "XXL"};
 
+        int[] intArray = {1, 1, 1, 1, 10};
         arrayHandler.addIndexToNumber(intArray);
 
         for (int i : intArray)
             System.out.println(i);
 
         for (int i = 0; i < intArray.length; i++)
-            if (i == intArray.length - 1)
-                System.out.println(intArray[i]);
+            if (i == 0)
+                System.out.print(intArray[i]);
             else
-                System.out.print(intArray[i] + ", ");
+                System.out.print(", " + intArray[i]);
 
 
+        System.out.println();
+
+
+        String[] stringArray = {"S", "M", "L", "XL", "XXL"};
         arrayHandler.concatenateIndexToWord(stringArray);
 
         for (String s : stringArray)
             System.out.println(s);
 
         for (int i = 0; i < stringArray.length; i++)
-            if (i == stringArray.length - 1)
-                System.out.println(stringArray[i]);
+            if (i == 0)
+                System.out.print(stringArray[i]);
             else
-                System.out.print(stringArray[i] + ", ");
+                System.out.print(", " + stringArray[i]);
 
     }
 }

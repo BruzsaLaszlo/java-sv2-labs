9fe0f0a75035f7e25631244cf21636a10763a2e6
Bruzsa László <lacika007@gmail.com>
2021-11-06 11:07:27 +0100

    Kódolási konvenciók

--- src/main/java/conventions/Car.java
+++ src/main/java/conventions/Car.java
@@ -2,18 +2,11 @@ package conventions;
 
 public class Car {
 
-    public void setCarType(String carType) {
-        this.carType = carType;
-    }
-
     private String carType;
     private String engineType;
     private int doors;
     private int persons;
 
-    public int getPersons() {
-        return persons;
-    }
 
     public Car(String carType, String engineType, int doors, int persons) {
         this.carType = carType;
@@ -22,32 +15,39 @@ public class Car {
         this.persons = persons;
     }
 
+    public void addModelName(String modelName) {
+        this.carType = carType + " " + modelName;
+    }
 
     public String getCarType() {
         return carType;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
     }
 
     public String getEngineType() {
         return engineType;
     }
 
-    public int getDoors() {
-        return doors;
-    }
-
     public void setEngineType(String engineType) {
         this.engineType = engineType;
+    }
+
+    public int getDoors() {
+        return doors;
     }
 
     public void setDoors(int doors) {
         this.doors = doors;
     }
 
-    public void setPersons(int persons) {
-        this.persons = persons;
+    public int getPersons() {
+        return persons;
     }
 
-    public void addModelName(String modelName) {
-        this.carType = carType + " " + modelName;
+    public void setPersons(int persons) {
+        this.persons = persons;
     }
 }
--- src/main/java/conventions/CarMain.java
+++ src/main/java/conventions/CarMain.java
@@ -2,8 +2,16 @@ package conventions;
 
 public class CarMain {
 
-    // TODO
+
     public static void main(String[] args) {
+
+        Car car = new Car("Skoda", "1.8T", 5, 5);
+        car.addModelName("Octavia");
+
+        System.out.println(car.getCarType());
+        System.out.println(car.getEngineType());
+        System.out.println(car.getDoors());
+        System.out.println(car.getPersons());
 
     }
 }

dffdcdacb9fd02b4f9eebb53a89c449ac1e72d85
Bruzsa László <lacika007@gmail.com>
2022-01-19 15:03:25 +0100

    combination pattern

---
+++ src/main/java/combinatorpatttern/Main.java
@@ -0,0 +1,35 @@
+package combinatorpatttern;
+
+import java.time.LocalDate;
+
+import static combinatorpatttern.CustomerRegistrationValidator.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Customer customer = new Customer(
+                "Alice",
+                "alice@gmail.com",
+                "+0898787879878",
+                LocalDate.of(2015, 1, 1)
+        );
+
+//        System.out.println(new CustomerValidatorService().isValid(customer));
+
+        // if valid we can store customer in db
+
+        // Using combinator pattern
+        ValidationResult result = isEmailValid()
+                .and(isPhoneNumberValid())
+                .and(isAnAdult())
+                .and(customer1 -> customer1.getEmail().equals("sdfsdf") ? ValidationResult.SUCCESS : ValidationResult.EMAIL_NOT_VALID)
+                .apply(customer);
+
+        System.out.println(result);
+
+        if (result != ValidationResult.SUCCESS) {
+            throw new IllegalStateException(result.name());
+        }
+
+    }
+}

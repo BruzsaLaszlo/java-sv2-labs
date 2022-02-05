d301c22bcda10283201c61a003600ffe9f043dc4
Bruzsa László <lacika007@gmail.com>
2022-02-06 00:38:29 +0100

    flyway

---
+++ src/test/resources/activitytracker/db/migration/V1__activity.sql
@@ -0,0 +1,8 @@
+create table activities
+(
+    id            bigint auto_increment
+        primary key,
+    start_time    datetime     not null,
+    activity_desc varchar(255) not null,
+    activity_type varchar(45)  not null
+);

e0b2b3fea2ed14717fb8011fb741c36920b68660
Bruzsa László <lacika007@gmail.com>
2022-02-06 23:07:49 +0100

    update table

--- src/test/resources/activitytracker/db/migration/V2__activity.sql
+++ src/test/resources/activitytracker/db/migration/V2__activity.sql
@@ -1,9 +1,10 @@
 create table track_point
 (
-    id          bigint auto_increment primary key,
+    id          bigint auto_increment,
     time        datetime not null,
     lat         double   not null,
     lon         double   not null,
     activity_id bigint,
+    PRIMARY KEY (id),
     FOREIGN KEY (activity_id) REFERENCES activities (id)
 );
